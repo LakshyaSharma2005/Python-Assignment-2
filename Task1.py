527d99ac63ae4c7124520893ffb321fd4ec37eb1
LakshyaSharma2005 <lakshyasharma7860@gmail.com>
2025-08-11 22:51:07 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,8 @@
+Evodd = int(input("Enter a Number: "))
+
+if(Evodd % 2 == 0):
+    print(Evodd ,"is an Even Number")
+elif(Evodd % 2 != 0):
+    print(Evodd ,"is an Odd Number")
+else:
+    print("Enter a valid Number")
